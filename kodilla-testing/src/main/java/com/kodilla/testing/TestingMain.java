19789ccf4612da28cb0177ea199117dee1c65a7e
diana duszak <dia.rutkowska@gmail.com>
2023-06-17 11:31:03 +0200

    6.2 TDD - Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,19 +1,19 @@
 package com.kodilla.testing;
+
 import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args) {
+        Calculator testCalculator = new Calculator();
+        int result = testCalculator.addAtoB(10,5);
+        int result2 = testCalculator.subtractAfromB(10,5);
 
-    Calculator testCalculator = new Calculator();
-    int result = testCalculator.addAtoB(10,5);
-    int result2 = testCalculator.subtractAfromB(10,5);
-
-    if( result == 15) {
-        System.out.println("test OK");
+        if( result == 15) {
+            System.out.println("test OK");
         } else {
             System.out.println("error");
         }
-    if( result2 == 5) {
-        System.out.println("test OK");
+        if( result2 == 5) {
+            System.out.println("test OK");
         } else {
             System.out.println("error");
         }
